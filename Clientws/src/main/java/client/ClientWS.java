d01380c678917a0ffbf33f6ac98d9d3121b29e6c
DalalBenallou <da_benallou@etu.enset-media.ac.ma>
2024-12-24 16:48:35 +0100

    V1

---
+++ Clientws/src/main/java/client/ClientWS.java
@@ -0,0 +1,16 @@
+package client;
+
+import proxy.BanqueService;
+import proxy.BanqueWs;
+import proxy.Compte;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        BanqueService proxy = new BanqueWs().getBanqueServicePort();
+        System.out.println(proxy.conversion(600));
+        Compte cp = proxy.getCompte(4);
+        System.out.println("------------------------------");
+        System.out.println(cp.getSolde());
+        System.out.println(cp.getCode());
+    }
+}
